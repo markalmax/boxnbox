5a722c05ba4fa987c00b1404579763623ff2c69b
markalmax <markalmax782@gmail.com>
2025-12-08 12:01:37 +0200

    Setup basic game

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,38 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+    public float jumpForce = 60f;
+    public float jumpVelocity = 5f;
+    public float jumpCount = 2f;
+    public float jumpCurrent = 0f;
+    public float jumpCD = 10f;
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb=GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
+        if (!IsOwner || !IsSpawned) return;
         
+        float input = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * input * Time.deltaTime * speed);
+        if(IsGrounded())
+        {
+            jumpCurrent = jumpCount;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)&& jumpCurrent > 0)
+        {
+            //rb.AddForce(new Vector2 (0,jumpForce));
+            rb.linearVelocityY = jumpVelocity;
+            jumpCurrent--;
+        }
+    }
+    bool IsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 1.1f, LayerMask.GetMask("Ground"));
     }
 }
