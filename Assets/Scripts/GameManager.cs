f93bf4d1b5bb9d1876d47d8138ab7e1edbbe519b
markalmax <markalmax782@gmail.com>
2026-05-11 23:56:22 +0300

    Added rotation and ping/internet testing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static GameManager instance;
+
     void Start()
     {
+        instance = this;
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
